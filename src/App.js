c875010165d85f98a752decea9e7f124bb2f69f2
Thamizharasan Devaraj <cdkthamizh27@gmail.com>
2020-11-25 19:04:40 +0530

    actual code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+const API_KEY = "e35855ae46802bdf5eed11304b734403	";
+const API_ID = "921ac74d";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [ingrediants, setIngrediants] = useState([]);
+  //const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    let url = `search?q=egg&app_id=${API_ID}&app_key=${API_KEY}`;
+
+    fetch(url, { mode: "no-cors" })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        return setIngrediants(data.hits);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  const handleClick = () => {
+    //setQuery(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="first">
+          <input
+            type="text"
+            value={value}
+            onChange={handleChange}
+            placeholder="Search for recipe..."
+          />
+          <button onClick={handleClick}>search</button>
+        </div>
+        <div className="wrapper">
+          {ingrediants.map((list) => {
+            return (
+              <div className="ingredients" key={list.recipe.label}>
+                <h2>{list.recipe.label}</h2>
+                <img
+                  className="image"
+                  src={list.recipe.image}
+                  alt="recipe images"
+                />
+                <div className="steps">
+                  {list.recipe.ingredientLines.map((steps, index) => {
+                    return <p key={index}>{steps}</p>;
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
 }
-
 export default App;
